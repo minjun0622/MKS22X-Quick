47e1cea6ad1925d1be48831fa6c8f693e9d4c8f0
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2019-03-14 20:48:29 -0400

    I just added partition. I just thought I had partition in my code but no

--- Quick.java
+++ Quick.java
@@ -19,26 +19,14 @@ public class Quick {
     }
     return dataH[0][k];
   }
-}
 
-/*return the value that is the kth smallest value of the array. k=0 is the smallest
- */
- public static int quickselect(int[] data, int k) {
-   if (k = 0) {
-     return data[0];
-   }
-   return quickselectH(data, int k);
- }
+public static int partition (int [] data, int start, int end){
 
- public static int quickselectH(int[] data, int k) {
-   return partition(data, k);
-
- }
 
 /*Modify the array to be in increasing order.
  */
  public static void quicksort(int[] data) {
-   return quicksortH(data, 0, data.length);
+   quicksortH(data, 0, data.length - 1);
  }
 
  /*Modify the array such that:
@@ -55,7 +43,41 @@ public class Quick {
      return;
    }
    int pivot = partition(data, lo, hi);
-   return quicksortH(data, lo, hi - 1); //hold up, which one am I returning?
-   return quicksortH(data, pivot + 1, hi);
+   quicksortH(data, lo, hi - 1);
+   quicksortH(data, pivot + 1, hi);
  }
+ public static void main(String[]args){
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        Quick.quicksort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
+  }
+}
 }
